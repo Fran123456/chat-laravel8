b3c44250f27d16d3dcf4f5cbf536a8b79887268e
Frank <navasfran98@gmail.com>
2021-04-23 18:55:00 -0600

    dump

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,24 +13,19 @@ class ComposerAutoloaderInit2e2c4074577ad0d6e6e56607ea7264d7
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit2e2c4074577ad0d6e6e56607ea7264d7', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit2e2c4074577ad0d6e6e56607ea7264d7', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit2e2c4074577ad0d6e6e56607ea7264d7::getInitializer($loader));
         } else {
